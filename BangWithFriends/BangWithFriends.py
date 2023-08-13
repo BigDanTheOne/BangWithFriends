3d7455ee14b966f3f97ffcc25534551eb1a3f10c
bigdan <230620ada>
2023-08-13 14:18:27 +0300

    Friend Component

--- BangWithFriends/BangWithFriends.py
+++ BangWithFriends/BangWithFriends.py
@@ -3,8 +3,8 @@ from rxconfig import config
 import requests
 import vkbottle
 import reflex as rx
-import json
-
+from .Friend import Friend
+from typing import List
 
 docs_url = "https://reflex.dev/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
@@ -12,14 +12,26 @@ filename = f"{config.app_name}/{config.app_name}.py"
 
 class State(rx.State):
     """The app state."""
-
     pass
 
 
+friends_mock: List[Friend] = [
+    Friend(*[1, 'm', 'Даниил Акопян', None, None, None, None, None,
+             'https://sun9-71.userapi.com/impg/CIHN6a0gLTe7QjtYH4U9F0aoexCXCXPgxEYFSg/ecis0a86N50.jpg?size=768x1024&quality=95&sign=fb56c34e6d3ab38bfe00c490b717f35c&type=album']),
+    Friend(*[2, 'm', 'Денис Мазур', None, None, None, None, None,
+             'https://sun9-57.userapi.com/impg/B3us9LpdBYlH2jDH5t0OHnQchg5v4XES6TPxTA/QUpiHgGnyOg.jpg?size=961x1280&quality=95&sign=0ef4a7c045743a9cd3ad7f5f0ef3f249&type=album'])
+]
+
+
 def index() -> rx.Component:
+
     return rx.fragment(
         rx.color_mode_button(rx.color_mode_icon(), float="right"),
         rx.vstack(
+            # rx.vstack(
+            #     friend.getComponent() for friend in friends_mock
+            # ),
+            friends_mock[0].getComponent(),
             rx.link(
                 "Log in with vk",
                 href="https://oauth.vk.com/authorize?client_id=51724129&redirect_uri=https://skemta.ru/vk-auth/&scope=2",
@@ -39,12 +51,14 @@ def index() -> rx.Component:
         ),
     )
 
+
 async def getVKFreinds(request):
     print(1)
     try:
         code = request.rel_url.query.get('code')
         print(1)
-        data = requests.get("https://oauth.vk.com/access_token?client_id=51724129&client_secret=M7lZNCwJ0IEWWNMhCBu0&redirect_uri=https://skemta.ru/vk-auth/" + code).json()
+        data = requests.get(
+            "https://oauth.vk.com/access_token?client_id=51724129&client_secret=M7lZNCwJ0IEWWNMhCBu0&redirect_uri=https://skemta.ru/vk-auth/" + code).json()
         vk_token = data['access_token']
         API = vkbottle.API(vk_token)
         vk_id = data['user_id']
@@ -59,7 +73,7 @@ async def getVKFreinds(request):
             }
         pass
     except:
-       pass
+        pass
     raise rx.redirect("/sucess")
 
 
